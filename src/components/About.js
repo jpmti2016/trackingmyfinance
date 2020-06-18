2213df82c6ce6fe85e3e3545f354f70f542836b8
Yampier Medina <jpmti2016@gmail.com>
2020-06-17 22:59:27 -0400

    Create some content to the about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./About.css";
 
 export default function About() {
   return (
@@ -8,20 +9,20 @@ export default function About() {
         style={{ paddingTop: "6rem", paddingBottom: "2rem" }}
       >
         <div className="container">
-          <h1 className="title">We help you take smart money desitions</h1>
-          <h2 className="subtitle">
-            A simple container to divide your page into{" "}
-            <strong>sections</strong>, like the one you're currently reading
-          </h2>
-        </div>
-      </section>
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Section</h1>
-          <h2 className="subtitle">
-            A simple container to divide your page into{" "}
-            <strong>sections</strong>, like the one you're currently reading
-          </h2>
+          <h2 className="title">About</h2>
+          <p>Keeping records of our expenses is a hard work.</p>
+          <p>
+            Also our receips, for everything, are in a continous treatening by
+            the amount of space that they need and the probability of lossing
+            them.
+          </p>
+          <p>
+            But event worse how to detect trends in our buying habits or how to
+            know the size of your budget still alive?
+          </p>
+          <p>
+            Try the app, create a new user or use user: test password: test2020
+          </p>
         </div>
       </section>
     </>
