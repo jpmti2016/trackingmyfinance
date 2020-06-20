b3d291b05e4ef98a726cd00e7fee5cd4279abd61
Yampier Medina <jpmti2016@gmail.com>
2020-06-20 04:11:05 -0400

    Fix that amazon refused to login cause the where to redirect url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
+import config from "./aws-exports";
 
 import UserContext from "./components/helpers/userContext";
 import useAmplifyAuth from "./components/helpers/useAmplifyAuth";
@@ -21,11 +21,49 @@ import ExpensePartTable from "./components/Expenses/ExpensePartTable";
 
 import ExpensePartForm from "./components/Expenses/Personal/ExpensePartForm";
 
-Amplify.configure(awsconfig);
-
-// Send analytics events to Amazon Pinpoint
-// Auth.configure(awsconfig)
-// Analytics.configure(awsconfig);
+const urlsIn = config.oauth.redirectSignIn.split(",");
+const urlsOut = config.oauth.redirectSignOut.split(",");
+const oauth = {
+  domain: config.oauth.domain,
+  scope: config.oauth.scope,
+  redirectSignIn: config.oauth.redirectSignIn,
+  redirectSignOut: config.oauth.redirectSignOut,
+  responseType: config.oauth.responseType,
+};
+const hasLocalhost = (hostname) =>
+  Boolean(
+    hostname.match(/localhost/) ||
+      hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/)
+  );
+const hasHostname = (hostname) =>
+  Boolean(hostname.includes(window.location.hostname));
+const isLocalhost = hasLocalhost(window.location.hostname);
+if (isLocalhost) {
+  urlsIn.forEach((e) => {
+    if (hasLocalhost(e)) {
+      oauth.redirectSignIn = e;
+    }
+  });
+  urlsOut.forEach((e) => {
+    if (hasLocalhost(e)) {
+      oauth.redirectSignOut = e;
+    }
+  });
+} else {
+  urlsIn.forEach((e) => {
+    if (hasHostname(e)) {
+      oauth.redirectSignIn = e;
+    }
+  });
+  urlsOut.forEach((e) => {
+    if (hasHostname(e)) {
+      oauth.redirectSignOut = e;
+    }
+  });
+}
+const configUpdate = config;
+configUpdate.oauth = oauth;
+Amplify.configure(configUpdate);
 
 function App() {
   const {
--- src/components/MySignIn.js
+++ src/components/MySignIn.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
 // const federated = {
 //     google_client_id: '389406440941-969d1fa65qgjoctdbhcgtcj87tfme7v2.apps.googleusercontent.com',
 //     amazon_client_id: '55u9ahaevm56mpnchti12s438l'
 // };
 
 export default function MySignIn() {
-    const theme = {
-        ...AmplifyTheme,
-        navBar: {
-            ...AmplifyTheme.navBar,
-            backgroundColor: "#BAA949",
-        },
-        oAuthSignInButton: {
-            ...AmplifyTheme.oAuthSignInButton,
-            padding: '0px'
-        },
-        button: {
-            ...AmplifyTheme.button,
-            backgroundColor: "var(--amazonOrange)"
-        },
-        sectionBody: {
-            ...AmplifyTheme.sectionBody,
-            padding: "5px"
-        },
-        sectionHeader: {
-            ...AmplifyTheme.sectionHeader,
-            backgroundColor: "var(--squidInk)"
-        }
-
-    }
-    return (
-        <Authenticator theme={theme} />
-    )
+  const theme = {
+    ...AmplifyTheme,
+    navBar: {
+      ...AmplifyTheme.navBar,
+    },
+    oAuthSignInButton: {
+      ...AmplifyTheme.oAuthSignInButton,
+      padding: "0px",
+    },
+    button: {
+      ...AmplifyTheme.button,
+      backgroundColor: "var(--amazonOrange)",
+    },
+    sectionBody: {
+      ...AmplifyTheme.sectionBody,
+      padding: "5px",
+    },
+    sectionHeader: {
+      ...AmplifyTheme.sectionHeader,
+      backgroundColor: "var(--squidInk)",
+    },
+  };
+  return (
+    <div
+      className="container"
+      style={{ paddingTop: "6rem", margin: "0 auto", maxWidth: "90vw" }}
+    >
+      <Authenticator theme={theme} />
+    </div>
+  );
 }
